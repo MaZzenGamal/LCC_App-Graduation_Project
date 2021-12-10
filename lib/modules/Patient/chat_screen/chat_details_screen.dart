21c51d0d7cc971beb2460e074114b61f171a33fd
MaZzenGamal <mazeng18@gmail.com>
2021-12-10 16:11:37 +0200

    design details of chat screen

--- lib/modules/Patient/chat_screen/chat_details_screen.dart
+++ lib/modules/Patient/chat_screen/chat_details_screen.dart
@@ -40,47 +40,131 @@ class ChatDetailsScreen extends StatelessWidget {
               )),
         ],
       ),
-      body: Column(
-        children: [
-          Spacer(),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey.withOpacity(0.5),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Align(
+              alignment: AlignmentDirectional.centerStart,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadiusDirectional.only(
+                    topStart: Radius.circular(10.0),
+                    topEnd: Radius.circular(10.0),
+                    bottomEnd: Radius.circular(10.0),
+                  )
                 ),
-                borderRadius: BorderRadius.circular(15.0),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 5.0,
+                  horizontal: 10.0),
+                  child: Text(
+                      'SUUUIIIIII'),
+                ),
               ),
-              clipBehavior: Clip.antiAliasWithSaveLayer,
-              child: Row(
-                children: [
-                  IconButton(onPressed: (){},
-                      icon: Icon(
-                        Icons.add_circle
-                      )),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: TextFormField(
-                        controller: messageController,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'write your message...'
+            ),
+            Align(
+              alignment: AlignmentDirectional.centerEnd,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue.withOpacity(0.3),
+                  borderRadius: BorderRadiusDirectional.only(
+                    topStart: Radius.circular(10.0),
+                    topEnd: Radius.circular(10.0),
+                    bottomStart: Radius.circular(10.0),
+                  )
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 5.0,
+                  horizontal: 10.0),
+                  child: Text(
+                      'SUUUUUUUUUUIIII'),
+                ),
+              ),
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey.withOpacity(0.5),
+                  ),
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                child: Row(
+                  children: [
+                    IconButton(onPressed: (){},
+                        icon: Icon(
+                          Icons.add_circle
+                        )),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: TextFormField(
+                          controller: messageController,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'write your message...'
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  IconButton(onPressed: (){},
-                      icon: Icon(
-                        Icons.send
-                      ))
-                ],
+                    IconButton(onPressed: (){},
+                        icon: Icon(
+                          Icons.send
+                        ))
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
+
+  Widget buildMessages() =>Align(
+    alignment: AlignmentDirectional.centerStart,
+    child: Container(
+      decoration: BoxDecoration(
+          color: Colors.grey[300],
+          borderRadius: BorderRadiusDirectional.only(
+            topStart: Radius.circular(10.0),
+            topEnd: Radius.circular(10.0),
+            bottomEnd: Radius.circular(10.0),
+          )
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+            vertical: 5.0,
+            horizontal: 10.0),
+        child: Text(
+            'shiiiiiiiiiiiiiiiiiit'),
+      ),
+    ),
+  );
+  Widget buildMyMessages() =>Align(
+    alignment: AlignmentDirectional.centerEnd,
+    child: Container(
+      decoration: BoxDecoration(
+          color: Colors.blue.withOpacity(0.3),
+          borderRadius: BorderRadiusDirectional.only(
+            topStart: Radius.circular(10.0),
+            topEnd: Radius.circular(10.0),
+            bottomStart: Radius.circular(10.0),
+          )
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+            vertical: 5.0,
+            horizontal: 10.0),
+        child: Text(
+            'shiiiiiiiiiiiiiiiiiit'),
+      ),
+    ),
+  );
 }
