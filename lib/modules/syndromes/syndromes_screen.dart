9992a8160f5ba13ca52c81961936711f85334d05
nada adel <nada56adel@gmail.com>
2022-03-04 13:52:02 +0200

    syndromes commit

--- lib/modules/syndromes/syndromes_screen.dart
+++ lib/modules/syndromes/syndromes_screen.dart
@@ -8,9 +8,21 @@ class SyndromesScreen extends StatefulWidget {
 }
 
 var age = 40;
-Map<dynamic,String>answer={0:"age",1:'',2:'',3:'',4:'',5:'',6:'',7:'',8:'',9:'',10:'',11:'',12:''};
 
 class _SyndromesScreenState extends State<SyndromesScreen> {
+  Map<dynamic, dynamic> answers = {0: age};
+  bool show1 = false,
+      show2 = false,
+      show3 = false,
+      show4 = false,
+      show5 = false,
+      show6 = false,
+      show7 = false,
+      show8 = false,
+      show9 = false,
+      show10 = false,
+      show11 = false,
+      show12 = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,110 +30,300 @@ class _SyndromesScreenState extends State<SyndromesScreen> {
         physics: BouncingScrollPhysics(),
         child: SafeArea(
           child: Container(
-            padding:const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
             child: Column(children: [
               defaultListView(
                 title: 'Are You Smoking?',
-                number:1,
+                onAnswer: (answer) => answers[1] = answer,
               ),
+              Visibility(
+                  visible: show1,
+                  child: Container(
+                      width: double.infinity,
+                      child: Text("please answer this question",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(color: Colors.red)))),
               const SizedBox(
-                height:20,
+                height: 20,
               ),
               defaultListView(
                 title: 'Are Your Fingers Yellow?',
-                number:2,
+                onAnswer: (answer) => answers[2] = answer,
               ),
+              Visibility(
+                  visible: show2,
+                  child: Container(
+                      width: double.infinity,
+                      child: Text("please answer this question",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(color: Colors.red)))),
               const SizedBox(
-                height:20,
+                height: 20,
               ),
               defaultListView(
                 title: 'Are You Anxiety?',
-                number:3,
+                onAnswer: (answer) => answers[3] = answer,
               ),
+              Visibility(
+                  visible: show3,
+                  child: Container(
+                      width: double.infinity,
+                      child: Text("please answer this question",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(color: Colors.red)))),
               const SizedBox(
-                height:20,
+                height: 20,
               ),
               defaultListView(
                 title: 'Are You Feeling Peer Pressure',
-                number:4,
+                onAnswer: (answer) => answers[4] = answer,
               ),
+              Visibility(
+                  visible: show4,
+                  child: Container(
+                      width: double.infinity,
+                      child: Text("please answer this question",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(color: Colors.red)))),
               const SizedBox(
-                height:20,
+                height: 20,
               ),
               defaultListView(
                 title: 'Are You Suffering From Any Chronic Disease?',
-                number:5,
+                onAnswer: (answer) => answers[5] = answer,
               ),
+              Visibility(
+                  visible: show5,
+                  child: Container(
+                      width: double.infinity,
+                      child: Text("please answer this question",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(color: Colors.red)))),
               const SizedBox(
-                height:20,
+                height: 20,
               ),
               defaultListView(
                 title: 'Are You Suffering From Any Fatigue?',
-                number:6,
+                onAnswer: (answer) => answers[6] = answer,
               ),
+              Visibility(
+                  visible: show6,
+                  child: Container(
+                      width: double.infinity,
+                      child: Text("please answer this question",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(color: Colors.red)))),
               const SizedBox(
-                height:20,
+                height: 20,
               ),
               defaultListView(
                 title: 'Are You Suffering From Allergy?',
-                number:7,
+                onAnswer: (answer) => answers[7] = answer,
               ),
+              Visibility(
+                  visible: show7,
+                  child: Container(
+                      width: double.infinity,
+                      child: Text("please answer this question",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(color: Colors.red)))),
               const SizedBox(
-                height:20,
+                height: 20,
               ),
               defaultListView(
                 title: 'Are You Suffering From Wheezing?',
-                number:8,
+                onAnswer: (answer) => answers[8] = answer,
               ),
+              Visibility(
+                  visible: show8,
+                  child: Container(
+                      width: double.infinity,
+                      child: Text("please answer this question",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(color: Colors.red)))),
               const SizedBox(
-                height:20,
+                height: 20,
               ),
               defaultListView(
                 title: 'Are You Drinking Alcohol?',
-                number:9,
+                onAnswer: (answer) => answers[9] = answer,
               ),
+              Visibility(
+                  visible: show9,
+                  child: Container(
+                      width: double.infinity,
+                      child: Text("please answer this question",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(color: Colors.red)))),
               const SizedBox(
-                height:20,
+                height: 20,
               ),
               defaultListView(
                 title: 'Are You Suffering From Coughing?',
-                number:10,
+                onAnswer: (answer) => answers[10] = answer,
               ),
+              Visibility(
+                  visible: show10,
+                  child: Container(
+                      width: double.infinity,
+                      child: Text("please answer this question",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(color: Colors.red)))),
               const SizedBox(
-                height:20,
+                height: 20,
               ),
               defaultListView(
                 title: 'Are You Suffering From Swallowing Difficulty?',
-                number:11,
+                onAnswer: (answer) => answers[11] = answer,
               ),
+              Visibility(
+                  visible: show11,
+                  child: Container(
+                      width: double.infinity,
+                      child: Text("please answer this question",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(color: Colors.red)))),
               const SizedBox(
-                height:20,
+                height: 20,
               ),
               defaultListView(
                 title: 'Are You Suffering From Chest Pain?',
-                number:12,
+                onAnswer: (answer) => answers[12] = answer,
               ),
+              Visibility(
+                  visible: show12,
+                  child: Container(
+                      width: double.infinity,
+                      child: Text("please answer this question",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(color: Colors.red)))),
               const SizedBox(
-                height:20,
+                height: 20,
               ),
               Container(
                 //style of the row that hold the name of list and icon
                 width: double.infinity,
-                padding:const EdgeInsets.only(right: 10),
+                padding: const EdgeInsets.only(right: 10),
                 decoration: BoxDecoration(
-                  color:  HexColor('4E51BF'),
-                    border: Border.all(
-                        color: HexColor('4E51BF')
-                    ),
-                    borderRadius:const BorderRadius.all(Radius.circular(20))),
+                    color: HexColor('4E51BF'),
+                    border: Border.all(color: HexColor('4E51BF')),
+                    borderRadius: const BorderRadius.all(Radius.circular(20))),
 
-                child: TextButton(onPressed: () {
-
-                }, child:const Text('SEND',
-                style:TextStyle(
-                  color: Colors.white
-                ),),
-
+                child: TextButton(
+                  onPressed: () {
+                    if (answers[1] == null)
+                      setState(() {
+                        show1 = true;
+                      });
+                    else {
+                      setState(() {
+                        show1 = false;
+                      });
+                    }
+                    if (answers[2] == null)
+                      setState(() {
+                        show2 = true;
+                      });
+                    else {
+                      setState(() {
+                        show2 = false;
+                      });
+                    }
+                    if (answers[3] == null)
+                      setState(() {
+                        show3 = true;
+                      });
+                    else {
+                      setState(() {
+                        show3 = false;
+                      });
+                    }
+                    if (answers[4] == null)
+                      setState(() {
+                        show4 = true;
+                      });
+                    else {
+                      setState(() {
+                        show4 = false;
+                      });
+                    }
+                    if (answers[5] == null)
+                      setState(() {
+                        show5 = true;
+                      });
+                    else {
+                      setState(() {
+                        show5 = false;
+                      });
+                    }
+                    if (answers[6] == null)
+                      setState(() {
+                        show6 = true;
+                      });
+                    else {
+                      setState(() {
+                        show6 = false;
+                      });
+                    }
+                    if (answers[7] == null)
+                      setState(() {
+                        show7 = true;
+                      });
+                    else {
+                      setState(() {
+                        show7 = false;
+                      });
+                    }
+                    if (answers[8] == null)
+                      setState(() {
+                        show8 = true;
+                      });
+                    else {
+                      setState(() {
+                        show8 = false;
+                      });
+                    }
+                    if (answers[9] == null)
+                      setState(() {
+                        show9 = true;
+                      });
+                    else {
+                      setState(() {
+                        show9 = false;
+                      });
+                    }
+                    if (answers[10] == null)
+                      setState(() {
+                        show10 = true;
+                      });
+                    else {
+                      setState(() {
+                        show10 = false;
+                      });
+                    }
+                    if (answers[11] == null)
+                      setState(() {
+                        show11 = true;
+                      });
+                    else {
+                      setState(() {
+                        show11 = false;
+                      });
+                    }
+                    if (answers[12] == null)
+                      setState(() {
+                        show12 = true;
+                      });
+                    else {
+                      setState(() {
+                        show12 = false;
+                      });
+                    }
+                  },
+                  child: const Text(
+                    'SEND',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
               ),
             ]),
