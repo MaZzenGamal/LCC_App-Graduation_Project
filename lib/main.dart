75ef14886c22a27a92d23df3822ae822f08a9f40
nada adel <nada56adel@gmail.com>
2022-04-22 23:24:07 +0200

    main commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,30 @@
 //@dart=2.9
-import 'package:bloc/bloc.dart';
+
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:graduation_project/layouts/app_layout/app_layout.dart';
-import 'package:graduation_project/modules/cancer%20_informations/cancer_info_screen.dart';
-import 'package:graduation_project/modules/cancer%20_informations/motivation_screen.dart';
-import 'package:graduation_project/modules/language/languages_screen.dart';
+import 'package:graduation_project/models/navkey.dart';
 import 'package:graduation_project/modules/login/cubit/login_cubit.dart';
 import 'package:graduation_project/modules/login/login_screen.dart';
-import 'package:graduation_project/modules/register/register_screen2.dart';
-import 'package:graduation_project/modules/syndromes/syndromes_screen.dart';
+import 'package:graduation_project/modules/reservation_screen/doctor_information_screen.dart';
+import 'package:graduation_project/modules/reservation_screen/doctors.dart';
+import 'package:graduation_project/modules/search_screen/search_screen.dart';
 import 'package:graduation_project/shared/block_observer.dart';
 import 'package:graduation_project/shared/components/conestants.dart';
 import 'package:graduation_project/shared/cubit/main_cubit.dart';
 import 'package:graduation_project/shared/cubit/main_states.dart';
 import 'package:graduation_project/shared/network/local/cash_helper.dart';
 import 'package:graduation_project/shared/styles/themes.dart';
-import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'layouts/app_layout/app_cubit.dart';
-import 'layouts/app_layout/states.dart';
-import 'modules/on_boarding/on_boarding_screen.dart';
 import 'modules/register/cubit/register_cubit.dart';
-import 'modules/register/register_screen1.dart';
-import 'modules/register/register_screen3.dart';
-import 'modules/reservation_screen/doctors.dart';
 import 'myTest/restart_screen.dart';
-import 'myTest/test_screen.dart';
-void main() async {
+import 'notification_service.dart';
+Future<void>main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   Bloc.observer = MyBlocObserver();
   //DioHelper.init();
   await CacheHelper.init();
-
   // token = CacheHelper.getData(key: 'token');
   uId = CacheHelper.getData(key: 'uId');
   // Widget widget;
@@ -48,10 +38,12 @@ void main() async {
   // }
   runApp(
     RestartWidget(
-      child:MyApp()));
+      child:const MyApp()));
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key key}) : super(key: key);
+
   @override
   State<MyApp> createState() => _MyAppState();
 }
@@ -59,27 +51,34 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   //const MyApp({Key? key}) : super(key: key);
   @override
-  void initState(){
-    super.initState();
-  }
-  @override
   Widget build(BuildContext context) {
     return  MultiBlocProvider(
       providers: [
         BlocProvider(create: (context)=>MainCubit(),),
-        BlocProvider(create: (context)=>AppCubit()..getUsers()..getUserData()..getComment(uId)),
+        BlocProvider(create: (context)=>AppCubit()..getUsers()..getUserData()),
         BlocProvider(create: (context)=>LoginCubit()),
         BlocProvider(create: (context)=>RegisterCubit(),),
       ],
-      child: BlocConsumer<MainCubit,MainStates>(
-        listener: (context,state){},
-        builder: (context,state)=>MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: lightTheme,
-          //darkTheme: darkTheme,
-          home:LoginScreen(),
-        ),
-      )
+      child: FutureBuilder(
+          future: fireInit(),
+    builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+      return BlocConsumer<MainCubit, MainStates>(
+        listener: (context, state) {},
+        builder: (context, state) =>
+            MaterialApp(
+              debugShowCheckedModeBanner: false,
+              theme: lightTheme,
+              //darkTheme: darkTheme,
+              navigatorKey: NavKey?.navkey,
+              routes:{
+                'profile':(context)=> SearchScreen(),
+
+              },
+              home:LoginScreen(),//DoctorsScreen(),//LoginScreen(), //LoginPage(),//LoginScreen(),//HomeTestScreen()//LoginScreen(),
+            ),
+      );
+    })
     );
+
   }
 }
