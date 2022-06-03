fcbbbbf6410a5c83515919cf99fd17d76afe4a08
nada adel <nada56adel@gmail.com>
2022-06-03 18:12:37 +0200

    main commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 //@dart=2.9
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graduation_project/layouts/app_layout/app_layout.dart';
-import 'package:graduation_project/models/navkey.dart';
 import 'package:graduation_project/modules/login/cubit/login_cubit.dart';
 import 'package:graduation_project/modules/login/login_screen.dart';
-import 'package:graduation_project/modules/reservation_screen/doctor_information_screen.dart';
-import 'package:graduation_project/modules/reservation_screen/doctors.dart';
-import 'package:graduation_project/modules/search_screen/search_screen.dart';
 import 'package:graduation_project/shared/block_observer.dart';
 import 'package:graduation_project/shared/components/conestants.dart';
 import 'package:graduation_project/shared/cubit/main_cubit.dart';
@@ -32,27 +29,29 @@ Future<void>main() async {
   //DioHelper.init();
   await CacheHelper.init();
   // token = CacheHelper.getData(key: 'token');
-  uId = CacheHelper.getData(key: 'uId');
+  //uId = CacheHelper.getData(key: 'uId');
   Widget widget;
 
   if(uId != null)
   {
-    widget = AppLayout();
+    widget = const AppLayout();
   }else
   {
     widget = LoginScreen();
   }
   runApp(
-    RestartWidget(
-      child: MyApp(widget)));
+      RestartWidget(
+          child: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key key}) : super(key: key);
+
   //const MyApp({Key key}) : super(key: key);
 
-    final Widget startWidget;
+ // final Widget startWidget;
 
-  MyApp(this.startWidget);
+  //MyApp(this.startWidget);
   @override
   State<MyApp> createState() => _MyAppState();
 }
@@ -62,40 +61,68 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return  MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context)=>MainCubit(),),
-        BlocProvider(create: (context)=>AppCubit()..getUserData()),
-        BlocProvider(create: (context)=>LoginCubit()),
-        BlocProvider(create: (context)=>RegisterCubit(),),
-      ],
-      child: FutureBuilder(
-          future: fireInit(context),
-    builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
-      final navkey= GlobalKey<NavigatorState>();
-      return BlocConsumer<MainCubit, MainStates>(
-        listener: (context, state) {},
-        builder: (context, state) =>
-            MaterialApp(
-              routes: {
-                'chatdoctor':(c)=>ChatDoctorScreen(),
-                'chatpatient':(c)=>ChatPatientScreen(),
-                'videoScreen':(c)=>VideoCallScreen(),
-                'audioScreen':(c)=>AudioCallScreen(),
-                'showPatientReservation':(c)=>ShowPatientReservation()
-              },
-              navigatorKey: navkey,
-              debugShowCheckedModeBanner: false,
-              theme: lightTheme,
-              //darkTheme: darkTheme,
-              home:FutureBuilder(
-                future:fcmInit(navkey) ,
-                builder: (context,_) {
-                  return AppLayout();
-                }
-              ),
-            ),
-      );
-    })
+        providers: [
+          BlocProvider(create: (context)=>MainCubit(),),
+          BlocProvider(create: (context)=>AppCubit()),
+          BlocProvider(create: (context)=>LoginCubit()),
+          BlocProvider(create: (context)=>RegisterCubit(),),
+        ],
+        child: FutureBuilder(
+            future: fireInit(context),
+            builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+              final navkey= GlobalKey<NavigatorState>();
+              return BlocConsumer<MainCubit, MainStates>(
+                listener: (context, state) {},
+                builder: (context, state) =>
+                    MaterialApp(
+                      routes: {
+                        'chatdoctor':(c)=>ChatDoctorScreen(),
+                        'chatpatient':(c)=>ChatPatientScreen(),
+                        'videoScreen':(c)=>VideoCallScreen(),
+                        'audioScreen':(c)=>const AudioCallScreen(),
+                        'showPatientReservation':(c)=>ShowPatientReservation()
+                      },
+                      navigatorKey: navkey,
+                      debugShowCheckedModeBanner: false,
+                      theme: lightTheme,
+                      //darkTheme: darkTheme,
+                      home:StreamBuilder(
+                        stream:FirebaseAuth.instance.idTokenChanges(),
+                        builder: (BuildContext context, AsyncSnapshot<User>snap) {
+                          print("the current user is ${FirebaseAuth.instance.currentUser}");
+                          return FutureBuilder(
+                              future:fcmInit(navkey) ,
+                              builder: (context,_) {
+                                if (snap.hasData) {
+                                  try {
+                                    context.read<AppCubit>()
+                                        .changeUserModel();
+                                    context.read<AppCubit>().currentIndex=2;
+                                  }
+                                  catch(c){
+                                    print("errror");
+                                  }
+                                  return const AppLayout();
+                                }
+                                else {
+                                  if(snap.hasError){
+                                  print(snap.error.toString());
+                                }
+                                else if(snap.data==null){
+                                     context.read<AppCubit>().currentIndex=2;
+                                     return LoginScreen();
+
+
+                                }
+                                }
+
+                              }
+                          );
+                        }
+                      ),
+                    ),
+              );
+            })
     );
 
   }
